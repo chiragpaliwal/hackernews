ba9176103e389a000ffdce83be41eb24e5d1ca0b
chiragpaliwal <chirag.paliwal06@gmail.com>
2018-09-28 16:48:42 +0530

    feature

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const DEFAULT_QUERY = 'redux';
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
 
 class App extends Component {
   constructor(props){
     super(props);
     this.state = {
+      result: null,
+      searchTerm: DEFAULT_QUERY,
     };
   }
 
